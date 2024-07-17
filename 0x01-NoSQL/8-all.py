08ab16c3c5df7bd1663f28b8a696e83790720668
gikonyo.mwema <gikonyo.mwema@gmail.com>
2024-07-16 19:34:41 -0700

    comments done

--- 0x01-NoSQL/8-all.py
+++ 0x01-NoSQL/8-all.py
@@ -1,11 +1,15 @@
+#!/usr/bin/env python3
+""" List All """
+
+
 def list_all(mongo_collection):
     """
     Lists all documents in the specified MongoDB collection
 
-    Args:
+     Args:
         mongo_collection: A PyMongo collectioin object.
 
-    Returns:
-        A list of documents (empty list if no documents found).
+     Returns:
+         A list of documents (empty list if no documents found).
     """
     return list(mongo_collection.find())
